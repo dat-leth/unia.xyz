a3dbcf04c4f84f6e44c84c59963fa9ec27817a59
Stefan Schmauch <53376250+Fanest@users.noreply.github.com>
2021-08-21 18:48:15 +0200

    python script for scraping now fully functional

--- scrapevorleungsverzeichnis.py
+++ scrapevorleungsverzeichnis.py
@@ -1,5 +1,8 @@
 import requests
+import toml
 from bs4 import BeautifulSoup
+from fuzzywuzzy import fuzz
+from fuzzywuzzy import process
 
 # Getting the website
 url = "https://www.uni-augsburg.de/de/fakultaet/fai/informatik/studium/lehre/bsc-inf2018/"
@@ -8,7 +11,47 @@ response = requests.get(url)
 # Extracting the links
 soup = BeautifulSoup(response.text, 'html.parser')
 courses = soup.find('div', class_="textEditorContent").findAll('a')
-results = {}
+uni_links = {}
 for course in courses:
-    results[course.find('b').string] = course.get('href')
-print(results)
+    uni_links[course.find('b').string] = course.get('href')
+
+
+# Open Toml file for redirects
+with open('./redirects.toml', 'r') as file:
+    redirects = toml.loads(file.read())
+
+uni_key_from = {
+    "Informatik 1": ["Platzhalter"],
+    "Programmierkurs": ["Java-Programmierkurs", "C-Programmierkurs"],
+    "Mathematik für Informatik I": ["Platzhalter"],
+    "Diskrete Strukturen und Logik": ["Platzhalter"],
+    "Lineare Algebra I": ["Lineare Algebra I"],
+    "Informatik 2": ["Informatik 2"],
+    "Mathematik für Informatik II": ["Mathematik für Informatiker II"],
+    "Einführung in die Theoretische Informatik": ["Einführung in die Theoretische Informatik"],
+    "Analysis I": ["Analysis 1"],
+    "Informatik 3": ["Platzhalter"],
+    "Stochastik für Informatiker": ["Platzhalter"],
+    "Datenbanksysteme": ["Platzhalter"],
+    "Systemnahe Informatik": ["Systemnahe Informatik"],
+    "Softwareprojekt": ["Softwareprojekt"],
+    "Grundlagen der Human-Computer-Interaction / Multimedia Grundlagen II":
+        ["Grundlagen der Human-Computer Interaction / Multimedia Grundlagen II"],
+    "Kommunikationssysteme": ["Platzhalter"],
+    "Grundlagen der Signalverarbeitung und des maschinellen Lernens / Multimedia Grundlagen I": ["Platzhalter"],
+}
+
+# Modify Toml representation
+for section in redirects["section"]:
+    if "link" in section:
+        for our_link in section["link"]:                            # Iterate all the redirects
+            if our_link["desc"] in uni_key_from:                    # Is our redirect one that can be updated?
+                for uni_link in uni_key_from[our_link["desc"]]:     # Iterate all possible Names the uni will give it
+                    if uni_link in uni_links:                       # Was it found on the Website as that name?
+                        our_link["link"] = uni_links[uni_link]      # Update the link we use
+                        print(our_link["desc"] + " <- " + uni_link) # Display the updated links for diagnostic purposes
+
+# Safe Toml file
+with open('./redirects.toml', 'w') as file:
+    file.write(toml.dumps(redirects))
+
